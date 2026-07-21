71b2c08c0af671fbca0e510d0be61321eb9662a5
Frederick Ziola <frederick.ziola@gmail.com>
2026-07-21 13:37:00 -0400

    Patch move constructor

--- firmware/include/util/mutex.hpp
+++ firmware/include/util/mutex.hpp
@@ -23,13 +23,23 @@ public:
     Guard(const Guard &) = delete;
     Guard &operator=(const Guard &) = delete;
 
+    Guard(Guard&& other)
+        : mutex(other.mutex),
+          object(other.object),
+          owns_lock(other.owns_lock)
+    {
+        other.owns_lock = false;
+    }
+
     ~Guard() {
-      xSemaphoreGive(this->mutex);
+      if (owns_lock)
+        xSemaphoreGive(this->mutex);
     }
 
     T *operator->() { return &this->object; }
     T &operator*() { return this->object; }
   private:
+    bool owns_lock = true;
     SemaphoreHandle_t mutex;
     T& object;
   };
